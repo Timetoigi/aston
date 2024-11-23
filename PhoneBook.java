af420c2f277e176f5e2f8418f9b4899d67c77b8c
Timetoigi <162355798+Timetoigi@users.noreply.github.com>
2024-11-23 20:56:53 +0300

    Update PhoneBook.java

--- PhoneBook.java
+++ PhoneBook.java
@@ -24,14 +24,12 @@ public class PhoneBook {
     public static void main(String[] args) {
         PhoneBook phoneBook = new PhoneBook();
 
-        // Добавляем записи
         phoneBook.add("Иванов", "123456");
         phoneBook.add("Петров", "654321");
         phoneBook.add("Иванов", "789012");
         phoneBook.add("Сидоров", "345678");
         phoneBook.add("Петров", "987654");
 
-        // Получаем номера по фамилии
         System.out.println("Записи для фамилии Иванов: " + phoneBook.get("Иванов"));
         System.out.println("Записи для фамилии Петров: " + phoneBook.get("Петров"));
         System.out.println("Записи для фамилии Сидоров: " + phoneBook.get("Сидоров"));
